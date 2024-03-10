1453bb92e339fe196ff86737bab261c0d0be3138
alexsanaliev <161589369+alexsanaliev@users.noreply.github.com>
2024-03-11 00:48:46 +0400

    Add files via upload

---
+++ Tasks.py
@@ -0,0 +1,171 @@
+    #Retail
+# products=[]
+# for i in range(2):
+#     print("Enter details for product:")
+#     product_name=input("Enter the product name:")
+#     product_color=input("Enter the item color:")
+#     product_price=float(input("Enter the price: $"))
+
+#     products.append({"name": product_name, "price": product_price})
+
+# print(products)
+
+
+    #Finances
+# total_value=0
+# for x in range(2):
+#     print("Type info for stock")
+
+#     btc=float(input("Type the number of btc: "))
+#     price=float(input("Type price per btc: "))
+
+#     stock_value=btc * price
+#     total_value = stock_value
+# print(total_value)
+
+
+    #Medicine
+# dosier=[]
+# for patient in range(5):
+#     patient_name=input("Please write down your name: ")
+#     patient_age=input(("Please write down your age: "))
+
+#     dosier.append({"name": patient_name, "age": patient_age})
+
+# print(dosier)
+
+
+    #Education
+# journal=[]
+# for s in range(8):
+#     studemt_name=input("Please write down your name: ")
+#     student_age=input("Please wite down your age: ")
+
+#     journal.append({"name": studemt_name, "age": student_age})
+
+# print(journal)
+
+
+    #Restaurant
+# foods = []
+
+# for i in range(4):
+#     print("Enter details for food product")
+#     food_name = input("Enter the food product name: ")
+#     food_price = float(input("Enter the food product price: $"))
+
+#     foods.append({"name": food_name, "price": food_price})
+# print(foods)
+
+
+    #Technology
+# developers = []
+
+# for i in range(4):
+#     print("Enter details for developer")
+#     name = input("Enter the name of the developer: ")
+#     age = input("Enter the age of the developer: ")
+#     language = input("Enter the preferred programming language: ")
+
+#     developers.append({"name": name, "age": age, "language": language})
+#     print(developers)
+
+
+    #Transport
+# distance=float(input("Please type distance in km's: "))
+# time=float(input("Please type how much time it took in hours "))
+
+# print(distance/time)
+
+
+    #Entertaiment
+# films = []
+
+# for i in range(4):
+#     print("Enter details for film")
+#     title = input("Enter the title of the film: ")
+#     production_date = input("Enter the production date of the film: ")
+
+#     films.append({"title": title, "production_date": production_date})
+
+# print(films)
+
+
+    #Buildings
+# houses = []
+
+# for i in range(4):
+#     print("Enter details for house: ")
+#     square_area = float(input("Enter the square area of the house (in square meters): "))
+#     price = float(input("Enter the price of the house: $"))
+
+#     houses.append({"square_area": square_area, "price": price})
+
+# print(houses)
+
+
+    #Ecomerce
+items = []
+
+# for i in range(7):
+#     print("Enter details for product: ")
+#     item_name = input("Enter the product name: ")
+#     item_price = float(input("Enter the product price: $"))
+
+#     products.append({"name": item_name, "price": item_price})
+
+# print(items)
+
+
+    #Agriculture
+# vegetables = []
+
+# for i in range(2):
+#     print("Enter details for vegetable: ")
+#     vegetable_name = input("Enter the vegetable name: ")
+#     vegetable_price = float(input("Enter the vegetable price per kilogram: $"))
+
+#     vegetables.append({"name": vegetable_name, "price": vegetable_price})
+
+# print(vegetables)
+
+
+    #Telecommunications
+# phones = []
+
+# for i in range(6):
+#     print("Enter details for phone: ")
+#     phone_name = input("Enter the phone name: ")
+#     phone_price = float(input("Enter the phone price: $"))
+
+#     phones.append({"name": phone_name, "price": phone_price})
+# print(phones)
+
+
+    #Traveling
+# airfare = float(input("Enter the cost of airfare: $"))
+# hotel = float(input("Enter the cost of hotel: $"))
+# car_rental = float(input("Enter the cost of car rental: $"))
+
+# total_cost=airfare+hotel+car_rental
+# print(total_cost)
+
+
+    #construction industry
+# wood_quantity = float(input("Enter the quantity of wood needed (in cubic meters): "))
+# wood_price = float(input("Enter the price of wood per cubic meter: $"))
+
+# cement_quantity = float(input("Enter the quantity of cement needed (in bags): "))
+# cement_price = float(input("Enter the price of cement per bag: $"))
+
+# sand_quantity = float(input("Enter the quantity of sand needed (in cubic meters): "))
+# sand_price = float(input("Enter the price of sand per cubic meter: $"))
+
+# iron_quantity = float(input("Enter the quantity of iron needed (in kilograms): "))
+# iron_price = float(input("Enter the price of iron per kilogram: $"))
+
+# total_cost = (wood_quantity * wood_price) + (cement_quantity * cement_price) + (sand_quantity * sand_price) + (iron_quantity * iron_price)
+
+# print(total_cost:)
+
+
