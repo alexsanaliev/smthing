24837d2bfad1c4ee92c9c67b4e21d36cf3b1f019
alexsanaliev <161589369+alexsanaliev@users.noreply.github.com>
2024-03-16 17:22:06 +0400

    Add files via upload

--- 3tasks.py
+++ 3tasks.py
@@ -1,57 +1,48 @@
-# Tasl1
-height=float(input("Enter your height: "))
+#Task!
+random_list=[2, 67, 43, 3, 7]
+e=random_list
 
-if height==155:
-    print("Your ideal weight range is: 43-48 kilos ")
+while e==random_list:
+    print(random_list)
 
-elif height==165:
-    print("Your ideal weight range is: 50-57 kilos ")
+#Task2
+letters=["a", "b", "c", "f", "g", "d", "e"]
+index=0
 
-elif height==175:
-    print("Your ideal weight range is: 60-65")
+while index < len(letters):
+    if letters[index] in ["f", "g"]:
+        index+=1
+        continue 
+    print(letters[index])
+    index += 1
 
-elif height==190:
-    print("Your ideal weight range is: 70-77")
 
-else:
-    print("Your ideal weight range is: 85-95") #Sərtdə formuldan istifadə qeyd olunmayib, ona gore bir təhər hesablayir,
-# yada butun cəki və boy araliglari əhatə etmə uchun daha uzun kod alinir 
+#Task3
+n=int(input("Enter number: "))
+c=0
+while c<=n:
+    print(c)
+    c+=1
+
+#Task4
+password=["alex777"]
+chances=3
+
+for code in range(chances):
+    log_in=input("Enter password: ")
+    if log_in in password:
+        print("accessed")
+        break
+    else:
+        chances-=1
+        if chances==0:
+         print("Blocked for 24 hours")
+        else:
+            print("Password is wrong, available chances: ", {chances})
+        
+
     
 
-# Task2
-shopping_list={
-    "t-shirt": 28,
-    "trousers": 52,
-    "shoes": 77,
-    "glasses": 12, 
-    "cap": 22}
-shopping_cart=[]
-print("Available products: ", shopping_list)
-choice=input("Add products to the shopping cart using comma: ").split(",")
-for items in choice:
 
- if items in shopping_list:
-    shopping_cart.append(items)
- else:
-     print("This product is not available in stock")
 
-print("Added to the cart", shopping_cart) 
-
-print("Total cost including taxes: ",)
-# UNFORTUNATELY DIDN`T COMPLETE THIS ONE, i`LL APRECIATE IT, IF YOU GONNA HELP ME
-
-# Task3
-car=0.50
-bus=1
-truck=2
-fuel=1
-
-wehicle=input("Type of wehicle? ")
-distance=float(input("Enteк covered distance in km`s "))
-
-if wehicle=="car":
-  print(distance*car, "$")
-elif wehicle=="bus":
-  print(distance*bus, "$")
-else:
-  print(distance*truck, "$")
+    
